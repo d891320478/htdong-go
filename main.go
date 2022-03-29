32b4487d824ba3aac983f13039c771f4990eadd1
htdong <dht31261@gmail.com>
2022-03-29 16:26:47 +0800

    c

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("/bin/bash", "-c", "tail -n 1000 /data/logs/knowledgecenterdata.log_20220113_1 | grep 21009")
-	output, _ := cmd.Output()
-	fmt.Println(string(output))
+	fmt.Println("a", "b", "c")
 }
