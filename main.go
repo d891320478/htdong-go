e939c5ff425a7f533f717491e8333f529af43ee8
htdong <dht31261@gmail.com>
2022-04-02 11:08:37 +0800

    c

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
-	fmt.Println("a", "b", "c")
+	// 创建文件/目录监听器
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer watcher.Close()
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					continue
+				}
+				// 打印监听事件
+				fmt.Println("event:", event)
+			case _, ok := <-watcher.Errors:
+				if !ok {
+					continue
+				}
+			}
+		}
+	}()
+	// 监听系统的根目录目录
+	err = watcher.Add("./")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = watcher.Add("/home/admin/conf")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for true {
+	}
 }
