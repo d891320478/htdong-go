7f7f3b5dc2593db6c53f7d10406c9b951424b006
htdong <dht31261@gmail.com>
2022-12-31 19:15:34 +0800

    c

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-CGO_ENABLED=0 GOOS=linux/windows/darwin GOARCH=amd64/arm64 go build -o Name -x src/Name.go
+CGO_ENABLED=0 GOOS=linux/windows/darwin GOARCH=amd64/arm64 go build -o build/Name -x src/Name.go
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o usr.exe -x src/Main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/usr.exe -x src/Main.go
