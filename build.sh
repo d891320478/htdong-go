a72b9a297414e45c605fd2d485cddb36ecb6f327
htdong <dht31261@gmail.com>
2023-03-30 14:42:55 +0800

    c

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ CGO_ENABLED=0 GOOS=linux/windows/darwin GOARCH=amd64/arm64 go build -o build/Nam
 
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/usr.exe -x src/Main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/getAllDanmu -x src/Main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/getAllDanmu.exe -x src/Main.go
