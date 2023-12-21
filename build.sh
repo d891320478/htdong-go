3f6215d52d50013f2614e9dd6955fc43467110ed
htdong <dht31261@gmail.com>
2023-12-21 17:12:43 +0800

    c

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
 CGO_ENABLED=0 GOOS=linux/windows/darwin GOARCH=amd64/arm64 go build -o build/Name -x src/Name.go
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/startBiliHttp -x src/Main.go
--- src/Main.go
+++ src/Main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"context"
-	"encoding/base64"
 	"fmt"
-	"net/http"
-	"os"
-	"os/exec"
 	"runtime/debug"
-	"strconv"
-	"time"
-
-	"github.com/redis/go-redis/v9"
 )
-
-func PathExists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
-}
-
-func wait(w http.ResponseWriter, req *http.Request) {
-	sleep, _ := strconv.ParseInt(req.URL.Query().Get("sleep"), 10, 64)
-	time.Sleep(time.Duration(sleep) * time.Second)
-	w.Write([]byte("wait"))
-}
-
-func checkBase64(ori string) []byte {
-	bt, err := base64.StdEncoding.DecodeString(ori)
-	if err != nil {
-		return []byte(ori)
-	}
-	return bt
-}
 
 func Throwable() {
 	err := recover()
@@ -42,36 +14,10 @@ func Throwable() {
 	fmt.Println(string(debug.Stack()))
 }
 
-func getServerFromSentinel() {
-	sentinel := redis.NewSentinelClient(&redis.Options{
-		Addr: "10.0.20.67:26379",
-		// Password: "donghaotian",
-	})
-	addr, err := sentinel.GetMasterAddrByName(context.Background(), "main").Result()
-	fmt.Println(addr)
-	fmt.Println(err)
-}
-
 func main() {
 	defer Throwable()
-	// redisService.Put("sso1", "sso1", 1, time.Minute)
-	// fmt.Println(redisService.Get("sso1"))
-	// getServerFromSentinel()
 	// smTest.Sm2WriteKeyFile()
 	// smTest.Sm2Encrypt()
-	// bililive.StartBiliHttp()
 	var val []string = nil
 	fmt.Println(len(val))
-}
-
-func runCmd(cmd string) string {
-	cmd1 := exec.Command("/bin/bash", "-c", cmd)
-	out1, err := cmd1.Output()
-	if err != nil {
-		fmt.Println(cmd)
-		fmt.Println(err)
-		return "0 a"
-	} else {
-		return string(out1)
-	}
 }
