f1a888443791cc31d79ad552ac9ce4e08e335a4b
htdong <dht31261@gmail.com>
2024-05-20 20:54:50 +0800

    c

--- src/Main.go
+++ src/Main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"runtime/debug"
 )
@@ -12,6 +14,12 @@ func Throwable() {
 	}
 	fmt.Println(err)
 	fmt.Println(string(debug.Stack()))
+}
+
+func Md5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
 
 func main() {
